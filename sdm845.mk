8e7218ca04ca1183370bbe26c3d09d36227a7883
saikiran jayanthi <jayanthi@codeaurora.org>
2018-02-26 16:21:12 +0530

    sdm845 - Donot enable  BroadcastRadio by default  Remove broadcast radio from manifest.xml and sdm845.xml
    Change-Id: I6526c6573f35aa4c9160440a2ea94b3aeee5b9c9

--- sdm845.mk
+++ sdm845.mk
@@ -144,7 +144,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
     android.hardware.configstore@1.0-service \
-    android.hardware.broadcastradio@1.0-impl \
     modetest
 
 # Vibrator
